213711084e6e918c4a88ee83bbc64a5d7ac2183d
haritha <hari.ece411@gmail.com>
2024-03-28 13:19:35 +0530

    jenkins file added

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ resource "aws_vpc" "airtelcare-vpc" {
 resource "aws_subnet" "airtelcare-subnet-1" {
   vpc_id = aws_vpc.airtelcare-vpc.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
   tags = {
     "Name" = "k8s-publicsubnet"
   }
@@ -26,6 +27,7 @@ resource "aws_subnet" "airtelcare-subnet-1" {
 resource "aws_subnet" "airtelcare-subnet-2" {
   vpc_id = aws_vpc.airtelcare-vpc.id
   cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
   tags = {
     "Name" = "k8s-publicsubnet2"
   }
